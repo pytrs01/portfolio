78846dd0e86d490a2ce5c29d5007c7e05f2cdc68
pytrs01 <114422165+pytrs01@users.noreply.github.com>
2022-10-13 22:39:28 -0500

    changes to about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import pfp from '../images/pfp.jpg'
+import './about.scss';
 
 function About() {
     return (
-        <div>
-            <h1>About Page</h1>
+        <div className='about-container'>
+            <div className='img-container'><img src={pfp} alt='profile pic' className='profile-pic'></img></div>
+            <div className='text-container'>
+            <p className='heading large'>Hi! I'm Pablo!</p>
+            <p className='about-me'>
+            I’m a Frontend Developer, currently at General Motors, based in Austin, TX! I graduated with a Bachelor of Science Degree in Information Sciences and Technology and a Smeal Business Certificate from Penn State. <br/>
+            <br/>
+            I enjoy coding beautiful and responsive websites. I have also dabbled in UI Design. I have a passion for designing and creating websites that help bridge the gap between people and technology! <br/>
+            <br/>
+            Please feel free to reach out to me! 
+            </p>
+            </div>
         </div>
     );
 }
